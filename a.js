d93a84f2de5561cd92bba64d17ad5343711c22eb
Shoxabbos <dukon3535@gmail.com>
2025-09-21 11:30:11 +0500

    full

---
+++ a.js
@@ -0,0 +1,36 @@
+// 1. Juft sonlar
+console.log(getEvenNumbers([1, 2, 3, 4, 5, 6])); 
+
+// 2. Ikki barobar
+console.log(doubleElements([1, 2, 3, 4])); 
+
+
+// 3. Yig‘indi
+console.log(sumArray([5, 10, 15])); 
+
+// 4. Massivni stringga aylantirish
+console.log(arrayToString([1, 2, 3, 4])); 
+
+
+// 5. Uzun so‘zlar
+console.log(filterLongWords(["apple", "kiwi", "banana", "pear"])); 
+
+
+// 6. Massivlarni birlashtirish
+console.log(mergeArrays([1, 2, 3], [4, 5, 6])); 
+
+
+// 7. Negativ sonlar
+console.log(getNegativeNumbers([10, -5, 20, -15, 30])); 
+
+
+// 8. Bir birlikka oshirish
+console.log(incrementArray([3, 5, 7])); 
+
+
+// 9. 10 soni bormi?
+console.log(hasTen([5, 10, 15, 20])); 
+
+
+// 10. "e" harflarini olib tashlash
+console.log(removeE("JavaScript Metodlari")); 
